358e191fad44ed3033d45a349f2bfaf216c4c585
prflorendo <pflorend@purdue.edu>
2024-02-24 15:40:27 -0500

    added install -y to all installs

--- zsh.sh
+++ zsh.sh
@@ -109,7 +109,7 @@ rm discord*
 # Install spotify
 curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt-get install -y spotify-client
 
 # Install vivaldi
 curl -O https://downloads.vivaldi.com/stable/vivaldi-stable_6.5.3206.63-1_amd64.deb
@@ -121,6 +121,5 @@ sudo apt upgrade
 
 # <group> ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown'
 # also install powertop + tlp for laptops
-# TODO: Add stuff to install vivaldi, discord, spotify
 
 sudo reboot
